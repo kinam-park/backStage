e3a2b728efc711cb09c9deac4460c10d809319d4
jpark <jpark@218.38.137.28>
2016-05-12 15:08:11 +0900

    j.park

--- backStage/src/main/java/com/bs/auth/AuthController.java
+++ backStage/src/main/java/com/bs/auth/AuthController.java
@@ -4,14 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.bs.models.User;
+import com.bs.models.UserVO;
+
 
 
 
@@ -19,48 +17,17 @@ import com.bs.models.User;
 
 public class AuthController {
 	private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-//	
-//	@Autowired
-//	private AuthService authService;
-	
 
-	@RequestMapping("/login.do")
-	public void login(HttpSession session) {
-		
-	}
 	
-	@RequestMapping("/loginFail.do")
-	public void loginFail(HttpSession session) {
-		
-	}
-	
-	@RequestMapping("/logout.do")
-	public void logout(HttpSession session) {
-		
-	}
-	
-////	
-	@RequestMapping("/loginView")
-	public ModelAndView loginView(HttpSession session) {
+	@RequestMapping("/getTest")
+	public ModelAndView getTest(UserVO userVO,HttpSession session) {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("auth/login");
+		
 		//UserVO result = authService.getTest(userVO);
-		//modelAndView.addObject("result",  result);
+		modelAndView.addObject("result",  "gg");
 		return modelAndView;
 	
 	}
-	
-	
-//	
-//	@RequestMapping("/getTest")
-//	public ModelAndView getTest(UserVO userVO,HttpSession session) {
-//		ModelAndView modelAndView = new ModelAndView();
-//		
-//		UserVO result = authService.getTest(userVO);
-//		modelAndView.addObject("result",  result);
-//		return modelAndView;
-//	
-//	}
 	
 	
 }
---
+++ backStage/src/main/java/com/bs/auth/AuthService.java
@@ -0,0 +1,21 @@
+package com.bs.auth;
+ 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class AuthService{
+	@Autowired
+	private AuthDao authDao;
+	
+	
+    
+}
