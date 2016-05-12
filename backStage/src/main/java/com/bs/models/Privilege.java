6845bfae4f1f47f81e4009d5ba995f479bde4b9a
jpark <jpark@218.38.137.28>
2016-05-12 14:46:37 +0900

    j.park

---
+++ backStage/src/main/java/com/bs/models/Privilege.java
@@ -0,0 +1,20 @@
+package com.bs.models;
+ 
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+
+public class Privilege {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
