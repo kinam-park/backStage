cdbb0a6eaf9ca88ab2187af5ee668fb26cced6b6
jpark <jpark@218.38.137.28>
2016-05-16 12:06:16 +0900

    j.park

---
+++ backStage/src/main/java/com/bs/admin/AdminController.java
@@ -0,0 +1,81 @@
+package com.bs.admin;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+	
+	@RequestMapping("/uploadImage")
+	public void uploadImage(MultipartFile mf,HttpSession session,HttpServletRequest request, HttpServletResponse response) {
+		ModelAndView modelAndView = new ModelAndView();
+		OutputStream out = null;
+        PrintWriter printWriter = null;
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        String dirPath = request.getSession().getServletContext().getRealPath("/")  + "uploadFile/";  
+        try{
+        	String fileName = mf.getOriginalFilename();
+            byte[] bytes = mf.getBytes();
+            String uploadPath = dirPath + fileName;//저장경로
+ 
+            out = new FileOutputStream(new File(uploadPath));
+            out.write(bytes);
+            String callback = request.getParameter("CKEditorFuncNum");
+ 
+            printWriter = response.getWriter();
+            String fileUrl = dirPath + fileName;//url경로
+ 
+            printWriter.println("<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction("
+                    + callback
+                    + ",'"
+                    + fileUrl
+                    + "','이미지를 업로드 하였습니다.'"
+                    + ")</script>");
+            printWriter.flush();
+ 
+        }catch(IOException e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (printWriter != null) {
+                    printWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+	 
+	     
+		
+		
+		
+		return;
+	
+	}
+	
+	
+}
---
+++ backStage/src/main/java/com/bs/admin/AdminService.java
@@ -0,0 +1,21 @@
+package com.bs.admin;
+ 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class AdminService{
+	@Autowired
+	private AdminDao adminDao;
+	
+	
+    
+}
