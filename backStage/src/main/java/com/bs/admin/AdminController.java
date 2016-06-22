a9c9d8d0cd0cc44c0b6d32c1e9e928d6f3f9f0ea
jpark <jpark@192.168.1.2>
2016-06-22 11:59:20 +0900

    j.park

--- backStage/src/main/java/com/bs/admin/AdminController.java
+++ backStage/src/main/java/com/bs/admin/AdminController.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,8 +28,8 @@ public class AdminController {
 
 	
 	@RequestMapping("/uploadImage")
-	public void uploadImage(MultipartFile mf,HttpSession session,HttpServletRequest request, HttpServletResponse response) {
-		ModelAndView modelAndView = new ModelAndView();
+	public void uploadImage(@RequestParam MultipartFile mf,HttpSession session,HttpServletRequest request, HttpServletResponse response) {
+//		ModelAndView modelAndView = new ModelAndView();
 		OutputStream out = null;
         PrintWriter printWriter = null;
         response.setCharacterEncoding("utf-8");
